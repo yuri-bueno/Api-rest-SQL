5f5fcec0aa7a69c471b991150fa015595efe98f4
yuri-bueno <yuri.bueno@live.com>
2022-07-20 23:23:55 -0300

    build

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,9 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _path = require('path');
+
+var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
@@ -12,25 +15,44 @@ require('./database');
 
 _dotenv2.default.config();
 
+const whiteList = [
+
+  '34.95.240.28',
+  'http://localhost:3000/',
+
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
-    constructor() {
-        this.app = _express2.default.call(void 0, );
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.app = _express2.default.call(void 0, );
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.app.use(_express2.default.urlencoded({ extended: true }));
-        this.app.use(_express2.default.json());
-        this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads')));
-    }
+  middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_express2.default.urlencoded({ extended: true }));
+    this.app.use(_express2.default.json());
+    this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads')));
+  }
 
-    routes() {
-        this.app.use('/', _homeRoutes2.default);
-        this.app.use('/alunos', _alunoRoutes2.default);
-        this.app.use('/users', _userRoutes2.default);
-        this.app.use('/token', _tokenRoutes2.default);
-        this.app.use('/fotos', _fotoRoutes2.default);
-    }
+  routes() {
+    this.app.use('/', _homeRoutes2.default);
+    this.app.use('/alunos', _alunoRoutes2.default);
+    this.app.use('/users', _userRoutes2.default);
+    this.app.use('/token', _tokenRoutes2.default);
+    this.app.use('/fotos', _fotoRoutes2.default);
+  }
 }
 exports. default = new App().app;
